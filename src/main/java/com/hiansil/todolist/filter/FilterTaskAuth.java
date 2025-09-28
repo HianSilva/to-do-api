6ee56a9de16394e86f873c9348689ebc9c6eb2f5
Hian Silva <hiansilacts@gmail.com>
2025-09-28 14:39:13 -0300

    chore: Remove unused import in FilterTaskAuth class

--- src/main/java/com/hiansil/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/hiansil/todolist/filter/FilterTaskAuth.java
@@ -11,7 +11,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Base64;
-import java.util.Objects;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
