2ab592db808860f7025d2b2d3c7a169bea0a0061
Hian Silva <hiansilacts@gmail.com>
2025-09-28 11:51:52 -0300

    feat: Implement user model and repository with user creation endpoint

---
+++ src/main/java/com/hiansil/todolist/user/UserController.java
@@ -0,0 +1,21 @@
+package com.hiansil.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping
+    public UserModel createUser(@RequestBody UserModel user) {
+        var createdUser = this.userRepository.save(user);
+        return createdUser;
+    }
+}
